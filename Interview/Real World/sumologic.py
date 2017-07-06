15fbe211925a8275327d8ca2e26902cbe425e347
arunraman <arunraman.19@gmail.com>
2017-07-06 09:56:29 -0700

    Adding mor

--- Interview/Real World/sumologic.py
+++ Interview/Real World/sumologic.py
@@ -11,3 +11,47 @@ names = 'Adam Brett Cari Donna Ethan Fred Grok Han Izzy Jay'.split()
 columns = 4
 
 print str(tableify(names, columns)).replace(', [',",\n [")
+
+# Question:
+
+# Given a string, find the longest substring that doesnt contain any repeating characters.
+# abaded - bade
+
+# aaaaa - a
+
+
+def lengthOfLongestSubstring(S):
+    # This should be replaced by an array (size = alphabet size).
+    last_occurrence = {}
+    longest_len_so_far = 0
+    longest_pos_so_far = 0
+    curr_starting_pos = 0
+    curr_length = 0
+
+    for k, c in enumerate(S):
+        l = last_occurrence.get(c, -1)
+        # If no repetition within window, no problems.
+        if l < curr_starting_pos:
+            curr_length += 1
+        else:
+            # Check if it is the longest so far
+            if curr_length > longest_len_so_far:
+                longest_pos_so_far = curr_starting_pos
+                longest_len_so_far = curr_length
+            # Cut the prefix that has repetition
+            curr_length -= l - curr_starting_pos
+            curr_starting_pos = l + 1
+        # In any case, update last_occurrence
+        last_occurrence[c] = k
+
+    # Maybe the longest substring is a suffix
+    if curr_length > longest_len_so_far:
+        longest_pos_so_far = curr_starting_pos
+        longest_len_so_far = curr_length
+
+    return S[longest_pos_so_far:longest_pos_so_far + longest_len_so_far]
+
+
+print lengthOfLongestSubstring("abadedaa")
+print lengthOfLongestSubstring("aaaaa")
+print lengthOfLongestSubstring("abcabc")
--- Interview/String/longest_substring.py
+++ Interview/String/longest_substring.py
@@ -25,16 +25,34 @@ def lengthOfLongestRepeatingSubstring(s):
 
 
 def lengthOfLongestNonRepeatingSubstring(s):
-    ans, start, end = 0, 0, 0
-    countDict = defaultdict(int)
-    for c in s:
-        end += 1
-        countDict[c] += 1
-        while countDict[s[start]] > 1:
-            countDict[s[start]] -= 1
-            start += 1
-        ans = max(ans, end - start)
-    print ans
+    last_occurrence = {}
+    longest_len_so_far = 0
+    longest_pos_so_far = 0
+    curr_starting_pos = 0
+    curr_length = 0
+
+    for k, c in enumerate(s):
+        l = last_occurrence.get(c, -1)
+        # If no repetition within window, no problems.
+        if l < curr_starting_pos:
+            curr_length += 1
+        else:
+            # Check if it is the longest so far
+            if curr_length > longest_len_so_far:
+                longest_pos_so_far = curr_starting_pos
+                longest_len_so_far = curr_length
+            # Cut the prefix that has repetition
+            curr_length -= l - curr_starting_pos
+            curr_starting_pos = l + 1
+        # In any case, update last_occurrence
+        last_occurrence[c] = k
+
+    # Maybe the longest substring is a suffix
+    if curr_length > longest_len_so_far:
+        longest_pos_so_far = curr_starting_pos
+        longest_len_so_far = curr_length
+
+    print s[longest_pos_so_far:longest_pos_so_far + longest_len_so_far]
 
 
 def lengthOfLongestSubstringKDistinct(s, k):
