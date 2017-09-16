4f3b285105f1e7ae3e7547bf4fe12a1bc2737c53
arunraman <arunraman.19@gmail.com>
2017-09-15 19:53:45 -0700

    Combining Codes

--- Interview/Matrix/num_of_paths.py
+++ Interview/Matrix/num_of_paths.py
@@ -1,7 +1,32 @@
-# Count the possible paths from top left to bottom right of a mXn matrix
-def num_paths_matrix_recursive(rows, cols):
-    if rows == 1 or cols == 1:
-        return 1
-    return num_paths_matrix_recursive(rows - 1, cols) + num_paths_matrix_recursive(rows, cols - 1)
 
-print num_paths_matrix_recursive(3, 3)
+
+
+class Solution(object):
+    # This uses DP approach
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[0] * n for x in range(m)]
+        dp[0][0] = 1
+        for x in range(m):
+            for y in range(n):
+                if x + 1 < m:
+                    dp[x + 1][y] += dp[x][y]
+                if y + 1 < n:
+                    dp[x][y + 1] += dp[x][y]
+        return dp[m - 1][n - 1]
+
+    # Count the possible paths from top left to bottom right of a mXn matrix
+    def num_paths_matrix_recursive(self, rows, cols):
+        if rows == 1 or cols == 1:
+            return 1
+        return self.num_paths_matrix_recursive(rows - 1, cols) \
+               + self.num_paths_matrix_recursive(rows, cols - 1)
+
+
+S = Solution()
+print S.uniquePaths(3, 3)
+print S.num_paths_matrix_recursive(3, 3)
