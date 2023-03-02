7fe3582fa6acf59a2620fe73e1e14bd8635bbee8
Arun Raman <arunvijay19@gmail.com>
2023-03-02 13:09:53 -0800

    Fix the spaces

--- Interview/Real World/Dropbox/C/getLinks.py
+++ Interview/Real World/Dropbox/C/getLinks.py
@@ -2,6 +2,7 @@ from threading import Condition, Lock, Thread
 import urllib2, urlparse
 from BeautifulSoup import BeautifulSoup
 
+
 class Crawler(object):
 
     def __init__(self, num_threads):
@@ -19,7 +20,7 @@ class Crawler(object):
 
         # Start threads.
         for i in xrange(self.number_of_threads):
-            thread = Thread(target=self.crawler)# args=(self.crawl_list, self.seen, self.waiting_threads))
+            thread = Thread(target=self.crawler)  # args=(self.crawl_list, self.seen, self.waiting_threads))
             thread.start()
             self.threads.append(thread)
 
@@ -30,31 +31,31 @@ class Crawler(object):
         return self.seen
 
     def crawler(self):
-            while True:
-            # Wait until we get a URL
-                with self.Lock:
-                    self.waiting_threads += 1
-                    #snum_threads_waiting[0] += 1
-                    while len(self.crawl_list) == 0:
-                    # If all the threads are waiting, then we're done (no more# URLs to crawl)
-                        if self.waiting_threads == self.number_of_threads:
-                            self.condition_var.notify_all()
-                            return
-                        self.condition_var.wait()
-                    url = self.crawl_list.pop()
-                    self.waiting_threads -= 1
+        while True:
+        #  Wait until we get a URL
+            with self.Lock:
+                self.waiting_threads += 1
+                # snum_threads_waiting[0] += 1
+                while len(self.crawl_list) == 0:
+                # If all the threads are waiting, then we're done (no more# URLs to crawl)
+                    if self.waiting_threads == self.number_of_threads:
+                        self.condition_var.notify_all()
+                        return
+                    self.condition_var.wait()
+                url = self.crawl_list.pop()
+                self.waiting_threads -= 1
 
-                # Download and parse the page.
-                #links = self.get_links(url)
-                links = self.findLinks(url)
+            # Download and parse the page.
+            #links = self.get_links(url)
+            links = self.findLinks(url)
 
-                # Add new URLs to 'seen' and 'crawl_list'.
-                with self.Lock:
-                    for link in links:
-                        if link not in self.seen:
-                            self.seen.add(link)
-                            self.crawl_list.append(link)
-                    self.condition_var.notify_all()
+            # Add new URLs to 'seen' and 'crawl_list'.
+            with self.Lock:
+                for link in links:
+                    if link not in self.seen:
+                        self.seen.add(link)
+                        self.crawl_list.append(link)
+                self.condition_var.notify_all()
 
     def findLinks(self, link):
         html_page = urllib2.urlopen(link)
